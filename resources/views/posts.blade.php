4a6740dfe13b6eabd161338bbb53dcbf8cb352e1
Mikhail <mikhail@lessons.kz>
2022-01-10 18:17:31 +0600

    Updated content in view

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -4,10 +4,25 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>My Blog</title>
+
+    <title>Posts</title>
+
     <link rel="stylesheet" href="/css/app.css">
 </head>
 <body>
-    <h1>Hello World! It is Mikahil!</h1>
+    <article>
+        <h1><a href="/post">First Post</a></h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque et molestie urna. Nulla suscipit semper est ac elementum. Nulla facilisi. Nam fermentum nisl a orci semper placerat. Etiam a diam sed dolor mattis tincidunt ut eu lorem. Praesent dapibus, justo nec eleifend faucibus, arcu ligula rhoncus ligula, vel egestas magna magna sed ex. Praesent in quam id est rhoncus pulvinar quis et sapien. Proin porttitor, dui eget porttitor maximus, nibh leo ornare est, a tempus enim enim non ligula. </p>
+    </article>
+
+    <article>
+        <h1><a href="/post">Second Post</a></h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque et molestie urna. Nulla suscipit semper est ac elementum. Nulla facilisi. Nam fermentum nisl a orci semper placerat. Etiam a diam sed dolor mattis tincidunt ut eu lorem. Praesent dapibus, justo nec eleifend faucibus, arcu ligula rhoncus ligula, vel egestas magna magna sed ex. Praesent in quam id est rhoncus pulvinar quis et sapien. Proin porttitor, dui eget porttitor maximus, nibh leo ornare est, a tempus enim enim non ligula. </p>
+    </article>
+
+    <article>
+        <h1><a href="/post">Third Post</a></h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque et molestie urna. Nulla suscipit semper est ac elementum. Nulla facilisi. Nam fermentum nisl a orci semper placerat. Etiam a diam sed dolor mattis tincidunt ut eu lorem. Praesent dapibus, justo nec eleifend faucibus, arcu ligula rhoncus ligula, vel egestas magna magna sed ex. Praesent in quam id est rhoncus pulvinar quis et sapien. Proin porttitor, dui eget porttitor maximus, nibh leo ornare est, a tempus enim enim non ligula. </p>
+    </article>
 </body>
 </html>
