6df421566e286906a89b02b96555fc0ca92ace74
Mikhail <mikhail@lessons.kz>
2022-01-11 15:20:25 +0600

    Created constructor, updated 'all()' and 'find()' functions

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -3,26 +3,32 @@
 namespace App\Models;
 
 use Illuminate\Support\Facades\File;
+use Spatie\YamlFrontMatter\YamlFrontMatter;
 
 class Post 
 {
+
+    public function __construct(public $title, public $excerpt, public $date, public $body, public $slug) {}
+
     public static function all() 
     {
-        $files =  File::files(resource_path("posts/"));
-    
-        return array_map(function($file) {
-            return $file->GetContents();
-        }, $files);
+        return collect(File::files(resource_path("posts/")))
+        ->map(function($file) {
+            return YamlFrontMatter::parseFile($file);
+        })
+        ->map(function($document) {
+            return new Post(
+                $document->title,
+                $document->excerpt,
+                $document->date,
+                $document->body(),
+                $document->slug,
+            );
+    });
     }
 
     public static function find($slug) 
     {
-        if(! file_exists($path = resource_path("posts/{$slug}.html"))) {
-            throw new ModelNotFoundException();
-        }
-
-        return cache()->remember("post.{$slug}", 3600, function() use($path) {
-            return file_get_contents($path);
-        });
+        return static::all()->firstWhere('slug', $slug);
     }
 }
