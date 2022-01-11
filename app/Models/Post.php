b60d7eae840bc15ee841d59f0437bc15ae17a1ae
Mikhail <mikhail@lessons.kz>
2022-01-11 14:14:06 +0600

    Created new method called 'all()' which returns all exist post

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,9 +2,21 @@
 
 namespace App\Models;
 
+use Illuminate\Support\Facades\File;
+
 class Post 
 {
-    public static function find($slug) {
+    public static function all() 
+    {
+        $files =  File::files(resource_path("posts/"));
+    
+        return array_map(function($file) {
+            return $file->GetContents();
+        }, $files);
+    }
+
+    public static function find($slug) 
+    {
         if(! file_exists($path = resource_path("posts/{$slug}.html"))) {
             throw new ModelNotFoundException();
         }
