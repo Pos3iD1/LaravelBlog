997fce46fa2c3b2adf03e9fc7af26ce7d4103d23
Mikhail <mikhail@lessons.kz>
2022-01-11 09:23:29 +0600

    Updated links to posts

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -11,17 +11,17 @@
 </head>
 <body>
     <article>
-        <h1><a href="/post">First Post</a></h1>
+        <h1><a href="/post/first-post">First Post</a></h1>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque et molestie urna. Nulla suscipit semper est ac elementum. Nulla facilisi. Nam fermentum nisl a orci semper placerat. Etiam a diam sed dolor mattis tincidunt ut eu lorem. Praesent dapibus, justo nec eleifend faucibus, arcu ligula rhoncus ligula, vel egestas magna magna sed ex. Praesent in quam id est rhoncus pulvinar quis et sapien. Proin porttitor, dui eget porttitor maximus, nibh leo ornare est, a tempus enim enim non ligula. </p>
     </article>
 
     <article>
-        <h1><a href="/post">Second Post</a></h1>
+        <h1><a href="/post/second-post">Second Post</a></h1>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque et molestie urna. Nulla suscipit semper est ac elementum. Nulla facilisi. Nam fermentum nisl a orci semper placerat. Etiam a diam sed dolor mattis tincidunt ut eu lorem. Praesent dapibus, justo nec eleifend faucibus, arcu ligula rhoncus ligula, vel egestas magna magna sed ex. Praesent in quam id est rhoncus pulvinar quis et sapien. Proin porttitor, dui eget porttitor maximus, nibh leo ornare est, a tempus enim enim non ligula. </p>
     </article>
 
     <article>
-        <h1><a href="/post">Third Post</a></h1>
+        <h1><a href="/post/third-post">Third Post</a></h1>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque et molestie urna. Nulla suscipit semper est ac elementum. Nulla facilisi. Nam fermentum nisl a orci semper placerat. Etiam a diam sed dolor mattis tincidunt ut eu lorem. Praesent dapibus, justo nec eleifend faucibus, arcu ligula rhoncus ligula, vel egestas magna magna sed ex. Praesent in quam id est rhoncus pulvinar quis et sapien. Proin porttitor, dui eget porttitor maximus, nibh leo ornare est, a tempus enim enim non ligula. </p>
     </article>
 </body>
